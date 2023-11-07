146e4e0c8e9a4c0f55e0acad053b87b5059a4558
AMANTOOP <113962696+AMANTOOP@users.noreply.github.com>
2023-11-07 17:51:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const session = require('express-session');
 const app = express();
 const port = 80;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'LoginForm')));
 
+app.use(session({
+  secret: '123qw',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+
 const users = [];
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'LoginForm', 'home.html'));
@@ -27,13 +36,24 @@ app.post('/register', (req, res) => {
   res.send('Registration successful! You can now <a href="/login">login</a>.');
 });
 
+
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
   const user = users.find((u) => u.email === email && u.password === password);
   if (user) {
-    res.send(`Login successful, ${user.email}!`);
+    req.session.user = user;
+    res.redirect('/age-calculator');
   } else {
     res.send('Login failed. Please try again or <a href="/register">register</a>.');
+  }
+});
+
+app.get('/age-calculator', (req, res) => {
+  const user = req.session.user;
+  if (!user) {
+    res.redirect('/login');
+  } else {
+    res.sendFile(path.join(__dirname, 'LoginForm', 'age-calculator.html'));
   }
 });
 
